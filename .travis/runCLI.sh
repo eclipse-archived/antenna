f2ddb216678773ad629c3781bec690d05eaaf5fa
Maximilian Huber <external.maximilian.huber2@Bosch-si.com>
2018-11-13 12:43:20 +0100

    feat(travis): add CLI and JDK9, JDK10 and JDK11 tests
    Signed-off-by: Maximilian Huber <external.maximilian.huber2@bosch-si.com>

---
+++ .travis/runCLI.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Copyright (c) Bosch Software Innovations GmbH 2018.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+
+set -e
+
+jar="$(readlink -f "$(dirname $0)/../antenna-basic-assembly/antenna-cli/target/antenna.jar")"
+echo "use the JAR=[$jar]"
+
+if [[ $# -eq 0 ]] ; then
+    echo 'no argument was passed'
+    exit 1
+fi
+dirToRunIn="$1"
+if [[ ! -d "$dirToRunIn" ]]; then
+    echo "$dirToRunIn does not exist"
+    exit 1
+fi
+
+java="java"
+if [[ $2 = "-debug" ]] ; then
+    java="$java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+fi
+
+set -x
+cd $dirToRunIn
+$java -jar $jar ./pom.xml
