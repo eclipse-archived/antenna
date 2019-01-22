5836f3cfc22d308235df3a33f57b38a7b55d63b3
Martin Idel <external.Martin.Idel@bosch-si.com>
2019-01-22 11:11:09 +0100

    features(policyengine) Add testing harness
    Signed-off-by: Martin Idel <external.Martin.Idel@bosch-si.com>

---
+++ antenna-testing/antenna-rule-engine-testing/src/main/java/org/eclipse/sw360/antenna/droolstesting/AbstractDroolsValidatorTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) Bosch Software Innovations GmbH 2019.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.eclipse.sw360.antenna.droolstesting;
+
+import org.eclipse.sw360.antenna.api.IEvaluationResult;
+import org.eclipse.sw360.antenna.model.artifact.Artifact;
+import org.eclipse.sw360.antenna.bundle.DroolsEvaluationResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+
+public class AbstractDroolsValidatorTest {
+
+    private List<Artifact> artifacts;
+    private List<IEvaluationResult> evaluations;
+
+    @Before
+    public void init() {
+        artifacts = new ArrayList<>();
+        evaluations = new ArrayList<>();
+    }
+
+    /**
+     * Add an artifact which will be scanned by this test
+     *
+     * @param artifact to be added to the test run
+     */
+    public void addArtifact(Artifact artifact) {
+        artifacts.add(artifact);
+    }
+
+    /**
+     * Add an evaluation policy used by the rules in the test. Usually, a rule will add artifacts that fail the rule to
+     * one or more evaluation results. Those results will later be scanned for failed artifacts. Therefore, all evaluation
+     * policy results used by the rule to be tested should be added.
+     *
+     * @param evaluationResult to be added to the test run
+     */
+    public void addEvaluationPolicyResult(DroolsEvaluationResult evaluationResult) {
+        evaluations.add(evaluationResult);
+    }
+
+    /**
+     * Given the file name of the rule to be used, creates a Rules object. Use together with RuleResultsAssert via:
+     *
+     * assertThat(rule("MyCustomRule.drl").whenRunning())...
+     *
+     * @param filename of the rule (e.g. "MyCustomRule.drl") in the rules project
+     * @return a Rule object
+     */
+    public Rule rule(String filename) {
+        return new Rule(this, filename, artifacts, evaluations);
+    }
+
+    /**
+     * Entry point for evaluating rules. Use as assertThat(rule("MyCustomRule.drl").whenRunning()
+     *
+     * @param results result of running a rule, obtain via rule("MyCustomRule.drl").whenRunning()
+     * @return
+     */
+    public static RuleResultsAssert assertThat(RuleResults results) {
+        return RuleResultsAssert.assertThat(results);
+    }
+}
---
+++ antenna-testing/antenna-rule-engine-testing/src/test/java/org/eclipse/sw360/antenna/droolstesting/DroolsAssertionsTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) Bosch Software Innovations GmbH 2019.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.eclipse.sw360.antenna.droolstesting;
+
+import org.eclipse.sw360.antenna.api.IEvaluationResult;
+import org.eclipse.sw360.antenna.model.artifact.Artifact;
+import org.eclipse.sw360.antenna.model.artifact.facts.ArtifactMatchingMetadata;
+import org.eclipse.sw360.antenna.model.xml.generated.MatchState;
+import org.eclipse.sw360.antenna.bundle.DroolsEvaluationResult;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.eclipse.sw360.antenna.droolstesting.AbstractDroolsValidatorTest.assertThat;
+
+public class DroolsAssertionsTest {
+
+    @Test(expected = AssertionError.class)
+    public void passesAllArtifactsFailsTestIfSomeArtifactIsInFailure() {
+        Artifact artifact = new Artifact();
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).passesAllArtifacts();
+    }
+
+    @Test
+    public void failsPassesTestIfSomeArtifactIsInFailure() {
+        Artifact artifact = new Artifact();
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).fails();
+    }
+
+    @Test
+    public void failsArtifactsPassesTestIfAllExpectedArtifactsFail() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact1, artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).failsArtifacts(artifact1, artifact2);
+    }
+
+    @Test
+    public void failsArtifactsPassesTestIfOnlyASubsetOfFailedArtifactsIsDeclaredAsFailure() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact1, artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).failsArtifacts(artifact1);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void failsArtifactsFailsTestIfSomeDeclaredFailureIsMissing() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).failsArtifacts(artifact1);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void failsExactlyArtifactsFailsTestIfOnlyASubsetOfFailedArtifactsIsDeclaredAsFailure() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact1, artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).failsExactlyArtifacts(artifact1);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void failsExactlyArtifactsFailsTestIfUndeclaredArtifactsFail() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact1, artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).failsExactlyArtifacts(artifact2);
+    }
+
+    @Test
+    public void withPolicyIdPassesTestIfDeclaredArtifactsFail() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+
+        IEvaluationResult droolsEvaluationResults = getTestIdEvaluation(artifact1, artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Collections.singletonList(droolsEvaluationResults));
+
+        assertThat(testing).failsArtifacts(artifact2).withPolicyId("TestId");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void whichAreExactlyThoseWithPolicyIdFailsTestIfSomeArtifactsAreMissing() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+        Artifact artifact3 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.SIMILAR));
+
+        IEvaluationResult droolsEvaluationResults1 = getTestIdEvaluation(artifact1, artifact3);
+        IEvaluationResult droolsEvaluationResults2 = getWrongTestIdEvaluation(artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Arrays.asList(droolsEvaluationResults1, droolsEvaluationResults2));
+
+        assertThat(testing).failsArtifacts(artifact1).whichAreExactlyThoseWithPolicyId("TestId");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void withPolicyIdFailsTestIfArtifactsFailWithWrongPolicyFail() {
+        Artifact artifact1 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.UNKNOWN));
+        Artifact artifact2 = new Artifact().addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+
+        IEvaluationResult droolsEvaluationResults1 = getTestIdEvaluation();
+        IEvaluationResult droolsEvaluationResults2 = getWrongTestIdEvaluation(artifact1, artifact2);
+
+        RuleResults testing = new RuleResults("MyCustomRule", Arrays.asList(droolsEvaluationResults1, droolsEvaluationResults2));
+
+        assertThat(testing).failsArtifacts(artifact1, artifact2).withPolicyId("TestId");
+    }
+
+    private IEvaluationResult getTestIdEvaluation(Artifact... artifacts) {
+        return new DroolsEvaluationResult("TestId", "For testing",
+                IEvaluationResult.Severity.INFO, new HashSet<>(Arrays.asList(artifacts)));
+    }
+
+    private IEvaluationResult getWrongTestIdEvaluation(Artifact... artifacts) {
+        return new DroolsEvaluationResult("WrongTestId", "For testing",
+                IEvaluationResult.Severity.FAIL, new HashSet<>(Arrays.asList(artifacts)));
+    }
+
+}
