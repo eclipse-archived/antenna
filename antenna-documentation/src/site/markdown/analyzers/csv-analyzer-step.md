9cd1f788804c29dec808b9bceb8e2bcc3b8dc975
Stephanie <stephanie.neubauer@bosch-si.com>
2019-06-12 08:09:12 +0200

    feat(csv): adding data model to csv analyzer
    Signed-off-by: Stephanie <stephanie.neubauer@bosch-si.com>
    Signed-off-by: Stephanie Neubauer <Stephanie.Neubauer@bosch-si.com>

--- antenna-documentation/src/site/markdown/analyzers/csv-analyzer-step.md
+++ antenna-documentation/src/site/markdown/analyzers/csv-analyzer-step.md
@@ -2,10 +2,9 @@
 This analyzer investigates a given *csv* file. The format of this csv file follows this format:
 
 ```
-"Artifact Id","Group Id","Version","License Short Name","License Long Name","File Name"
-commons-csv,org.apache.commons,1.4,Apache-2.0,Apache Software License 2.0,commons-csv.jar
+"Artifact Id","Group Id","Version","License Short Name","License Long Name","File Name","Coordinate Type","Observed License Short Name","Observed License Long Name","Source URL","Release Tag URL","Software Heritage URL"
+commons-csv,org.apache.commons,1.4,Apache-2.0,Apache Software License 2.0,commons-csv.jar,mvn,Apache-2.0,Apache Software License 2.0,http://archive.apache.org/dist/commons/csv/source/commons-csv-1.4-src.zip,url,url
 ```
-
 
 ### How to use
 Add the following step into the `<analyzers>` section of your workflow.xml
@@ -24,5 +23,6 @@ Add the following step into the `<analyzers>` section of your workflow.xml
 
 #### Explanation of parameters
 * `file.path`: Destination of a CSV file.
+
 
 
--- core/core-workflow-steps/src/main/java/org/eclipse/sw360/antenna/workflow/analyzers/CsvAnalyzer.java
+++ core/core-workflow-steps/src/main/java/org/eclipse/sw360/antenna/workflow/analyzers/CsvAnalyzer.java
@@ -19,10 +19,12 @@ import org.eclipse.sw360.antenna.api.exceptions.AntennaException;
 import org.eclipse.sw360.antenna.api.workflow.ManualAnalyzer;
 import org.eclipse.sw360.antenna.api.workflow.WorkflowStepResult;
 import org.eclipse.sw360.antenna.model.artifact.Artifact;
-import org.eclipse.sw360.antenna.model.artifact.facts.ArtifactMatchingMetadata;
-import org.eclipse.sw360.antenna.model.artifact.facts.DeclaredLicenseInformation;
+import org.eclipse.sw360.antenna.model.artifact.facts.*;
+import org.eclipse.sw360.antenna.model.artifact.facts.dotnet.DotNetCoordinates;
 import org.eclipse.sw360.antenna.model.artifact.facts.java.ArtifactPathnames;
+import org.eclipse.sw360.antenna.model.artifact.facts.java.BundleCoordinates;
 import org.eclipse.sw360.antenna.model.artifact.facts.java.MavenCoordinates;
+import org.eclipse.sw360.antenna.model.artifact.facts.javaScript.JavaScriptCoordinates;
 import org.eclipse.sw360.antenna.model.xml.generated.License;
 import org.eclipse.sw360.antenna.model.xml.generated.MatchState;
 
@@ -41,6 +43,12 @@ public class CsvAnalyzer extends ManualAnalyzer {
     private static final String LICENSE_SHORT_NAME = "License Short Name";
     private static final String LICENSE_LONG_NAME = "License Long Name";
     private static final String PATH_NAME = "File Name";
+    private static final String COORDINATE_TYPE = "Coordinate Type";
+    private static final String OBSERVED_LICENSE_SHORT_NAME = "Observed License Short Name";
+    private static final String OBSERVED_LICENSE_LONG_NAME = "Observed License Long Name";
+    private static final String SOURCE_URL = "Source URL";
+    private static final String RELEASE_ARTIFACT_URL = "Release Tag URL";
+    private static final String SWH_URL = "Software Heritage URL";
 
     public CsvAnalyzer() {
         this.workflowStepOrder = 500;
@@ -52,22 +60,18 @@ public class CsvAnalyzer extends ManualAnalyzer {
         List<CSVRecord> records = getRecords();
 
         for (CSVRecord record : records) {
-            MavenCoordinates coordinates = new MavenCoordinates(record.get(NAME), record.get(GROUP), record.get(VERSION));
-
-            License license = new License();
-            license.setName(record.get(LICENSE_SHORT_NAME));
-            license.setLongName(record.get(LICENSE_LONG_NAME));
-
             String pathName = record.get(PATH_NAME);
             String absolutePathName = Paths.get(pathName).isAbsolute()
                     ? Paths.get(pathName).toString()
                     : baseDir.resolve(Paths.get(pathName)).toAbsolutePath().toString();
 
             Artifact artifact = new Artifact(getName())
-                    .addFact(coordinates)
-                    .addFact(new DeclaredLicenseInformation(license))
+                    .addFact(new DeclaredLicenseInformation(
+                            createLicense(record.get(LICENSE_SHORT_NAME), record.get(LICENSE_LONG_NAME))))
                     .addFact(new ArtifactPathnames(absolutePathName))
-                    .addFact(new ArtifactMatchingMetadata(MatchState.EXACT));
+                    .addFact(new ArtifactMatchingMetadata(MatchState.EXACT))
+                    .addFact(createCoordinates(record));
+            addOptionalArtifactFacts(record, artifact);
 
             artifacts.add(artifact);
         }
@@ -99,5 +103,46 @@ public class CsvAnalyzer extends ManualAnalyzer {
     @Override
     public String getName() {
         return "CSV";
+    }
+
+    private License createLicense(String short_name, String long_name) {
+        License license = new License();
+        license.setName(short_name);
+        license.setLongName(long_name);
+
+        return license;
+    }
+
+    private ArtifactCoordinates createCoordinates(CSVRecord record) {
+        String type = record.isMapped(COORDINATE_TYPE) ? record.get(COORDINATE_TYPE) : "mvn";
+
+        switch (type) {
+            case "mvn":
+                return new MavenCoordinates(record.get(NAME), record.get(GROUP), record.get(VERSION));
+            case "dotnet":
+                return new DotNetCoordinates(record.get(NAME), record.get(VERSION));
+            case "javascript":
+                return new JavaScriptCoordinates(record.get(NAME), record.get(GROUP), record.get(VERSION));
+            case "bundle":
+                return new BundleCoordinates(record.get(NAME), record.get(VERSION));
+            default:
+                return new GenericArtifactCoordinates(record.get(NAME), record.get(VERSION));
+        }
+    }
+
+    private void addOptionalArtifactFacts(CSVRecord record, Artifact artifact) {
+        if (record.isMapped(OBSERVED_LICENSE_SHORT_NAME) && record.isMapped(OBSERVED_LICENSE_LONG_NAME)) {
+            artifact.addFact(new ObservedLicenseInformation(
+                    createLicense(record.get(OBSERVED_LICENSE_SHORT_NAME), record.get(OBSERVED_LICENSE_LONG_NAME))));
+        }
+        if (record.isMapped(SOURCE_URL)) {
+            artifact.addFact(new ArtifactSourceUrl(record.get(SOURCE_URL)));
+        }
+        if (record.isMapped(RELEASE_ARTIFACT_URL)) {
+            artifact.addFact(new ArtifactReleaseTagURL(record.get(RELEASE_ARTIFACT_URL)));
+        }
+        if (record.isMapped(SWH_URL)) {
+            artifact.addFact(new ArtifactSoftwareHeritageURL(record.get(SWH_URL)));
+        }
     }
 }
