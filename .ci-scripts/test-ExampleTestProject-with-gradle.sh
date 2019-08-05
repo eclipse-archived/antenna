8e6f295234c08726c169d3df7291fbd15f5f25a2
Maximilian Huber <external.maximilian.huber2@bosch-si.com>
2019-08-05 12:55:01 +0200

    test(ci): replace test cases with scripts
    Signed-off-by: Maximilian Huber <external.maximilian.huber2@bosch-si.com>

---
+++ .ci-scripts/test-ExampleTestProject-with-gradle.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Copyright (c) Bosch Software Innovations GmbH 2019.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+
+set -ex
+
+cd "$(dirname "$0")/.."
+
+tmpdir=$(mktemp -d)
+trap 'rm -rf $tmpdir' EXIT
+cp -r example-projects/example-project/ $tmpdir/example-project/
+rm -rf $tmpdir/example-project/target
+rm -rf $tmpdir/example-project/build
+cp assembly/gradle-plugin/gradlew $tmpdir/example-project/
+cp -r assembly/gradle-plugin/gradle $tmpdir/example-project/
+pushd $tmpdir/example-project
+./gradlew analyze
+popd
+java -jar core/frontend-stubs-testing/target/antenna-test-project-asserter.jar ExampleTestProject $tmpdir/example-project/build
