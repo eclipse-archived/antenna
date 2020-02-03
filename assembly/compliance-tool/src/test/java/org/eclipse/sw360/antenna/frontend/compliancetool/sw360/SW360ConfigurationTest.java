3cd8499f423059ae0cf52964003e4cea6355f30e
Stephanie Neubauer <Stephanie.Neubauer@bosch.io>
2020-02-03 14:38:05 +0100

    Resolve environment variables in properties file, compliance tool
    Issue: #411
    
    Adds resolving of variables prefixed with a $ to environment
    variables and the required tests.
    
    Additionally refactors some methods.
    
    Signed-off-by: Stephanie Neubauer <Stephanie.Neubauer@bosch.io>

--- assembly/compliance-tool/src/test/java/org/eclipse/sw360/antenna/frontend/compliancetool/sw360/SW360ConfigurationTest.java
+++ assembly/compliance-tool/src/test/java/org/eclipse/sw360/antenna/frontend/compliancetool/sw360/SW360ConfigurationTest.java
@@ -10,15 +10,26 @@
  */
 package org.eclipse.sw360.antenna.frontend.compliancetool.sw360;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SW360ConfigurationTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test(expected = IllegalArgumentException.class)
     public void testConfigurationWithNonExistentFile() {
         new SW360Configuration(new File("non-existent-file"));
@@ -44,5 +55,32 @@ public class SW360ConfigurationTest {
         assertThat(configuration.getProperties().get("delimiter")).isEqualTo(";");
         assertThat(configuration.getProperties().get("sw360updateReleases")).isEqualTo("true");
         assertThat(configuration.getProperties().get("sw360uploadSources")).isEqualTo("false");
+    }
+
+    @Test
+    public void testConfigurationWithVariables() throws IOException {
+        File file = folder.newFile("fake.properties");
+        Map<String, String> envVars = System.getenv();
+        List<String> propertyLines = envVars.keySet().stream()
+                .map(s -> s + "=" + "${" + s + "}" + System.lineSeparator())
+                .limit(5)
+                .collect(Collectors.toList());
+        Files.write(file.toPath(), propertyLines);
+
+        Map<String, String> propertiesMap = ComplianceFeatureUtils.mapPropertiesFile(file);
+        assertThat(propertiesMap.entrySet().stream()
+                .allMatch(e -> e.getValue().equals(System.getenv(e.getKey())))).isTrue();
+    }
+
+    @Test
+    public void testConfigurationWithUnknownEnvironmentVariables() {
+        String key = "my.property";
+        String value = "${non existing environment variable?!}";
+        Properties properties = new Properties();
+        properties.setProperty(key, value);
+
+        Map<String, String> map = ComplianceFeatureUtils.propertiesToMap(properties, ComplianceFeatureUtils::mapEnvironmentVariable);
+        assertThat(map.size()).isEqualTo(1);
+        assertThat(map.get(key)).isEqualTo(value);
     }
 }
