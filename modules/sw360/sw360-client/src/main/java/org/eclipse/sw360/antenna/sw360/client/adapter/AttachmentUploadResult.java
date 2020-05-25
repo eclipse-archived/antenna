99bdcbaaa509324f46974f78c28270cf0b5bc99e
Oliver Heger <60134199+oheger-bosch@users.noreply.github.com>
2020-05-25 12:10:36 +0200

    Added data classes for attachment uploads.
    Issue: eclipse#426.
    
    These classes represent requests and results to upload multiple
    attachments. In the current design, multiple attachments can be
    uploaded at once using a map; however, there is no way to find out
    whether a single upload was successful or not - if one upload fails,
    the complete request fails.
    
    The new classes simplify the definition of multi-upload requests and
    provide fine-grained information about the results.
    
    Signed-off-by: Oliver Heger <60134199+oheger-bosch@users.noreply.github.com>

---
+++ modules/sw360/sw360-client/src/main/java/org/eclipse/sw360/antenna/sw360/client/adapter/AttachmentUploadResult.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) Bosch.IO GmbH 2020.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.sw360.antenna.sw360.client.adapter;
+
+import org.eclipse.sw360.antenna.sw360.client.rest.resource.releases.SW360Release;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * <p>
+ * A data class holding the result of a (multi) attachment upload request.
+ * </p>
+ * <p>
+ * When uploading multiple attachments using a single request, every single
+ * upload might fail. With this class it is possible to get detailed
+ * information about all the uploads that were made and which of them were
+ * successful. The updated {@link SW360Release} object is also available.
+ * </p>
+ */
+public final class AttachmentUploadResult {
+    /**
+     * Stores the target entity of the upload operation.
+     */
+    private final SW360Release target;
+
+    /**
+     * Stores information about the attachments that could be uploaded.
+     */
+    private final Set<AttachmentUploadRequest.Item> successfulUploads;
+
+    /**
+     * A map with information about attachments that could not be uploaded and
+     * the reasons for the failed uploads.
+     */
+    private final Map<AttachmentUploadRequest.Item, Throwable> failedUploads;
+
+    /**
+     * Creates a new instance of {@code AttachmentUploadResult} that references
+     * the given release.
+     *
+     * @param target the {@code SW360Release} that was the target for uploads
+     */
+    public AttachmentUploadResult(SW360Release target) {
+        this(target, Collections.emptySet(), Collections.emptyMap());
+    }
+
+    /**
+     * Internal constructor to create an instance of
+     * {@code AttachmentUploadResult} with all information.
+     *
+     * @param target            the {@code SW360Release} that was the target for uploads
+     * @param successfulUploads a set with successful uploads
+     * @param failedUploads     a map with failed uploads
+     */
+    private AttachmentUploadResult(SW360Release target,
+                                   Set<AttachmentUploadRequest.Item> successfulUploads,
+                                   Map<AttachmentUploadRequest.Item, Throwable> failedUploads) {
+        this.target = target;
+        this.successfulUploads = successfulUploads;
+        this.failedUploads = failedUploads;
+    }
+
+    /**
+     * Returns the release that is the target of the upload operation.
+     *
+     * @return the updated release to which attachments have been uploaded
+     */
+    public SW360Release getTarget() {
+        return target;
+    }
+
+    /**
+     * Returns a flag whether the whole upload operation was successful. If
+     * this method returns <strong>true</strong>, all the single attachment
+     * uploads have been successful; otherwise, there was at least one failure.
+     * More information about successful and failed uploads is then available
+     * through the other methods of this class.
+     *
+     * @return a flag whether all attachments could be uploaded successfully
+     */
+    public boolean isSuccess() {
+        return failedUploads.isEmpty();
+    }
+
+    /**
+     * Returns a set with information about all the attachment items that could
+     * be uploaded successfully.
+     *
+     * @return a set with successfully uploaded attachment items
+     */
+    public Set<AttachmentUploadRequest.Item> successfulUploads() {
+        return successfulUploads;
+    }
+
+    /**
+     * Returns a map with information about all the attachment items that could
+     * not be uploaded successfully. For each item whose upload caused a
+     * failure, the corresponding exception can be queried.
+     *
+     * @return a map with the failed attachment items and the corresponding
+     * exceptions
+     */
+    public Map<AttachmentUploadRequest.Item, Throwable> failedUploads() {
+        return failedUploads;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AttachmentUploadResult result = (AttachmentUploadResult) o;
+        return Objects.equals(getTarget(), result.getTarget()) &&
+                successfulUploads.equals(result.successfulUploads) &&
+                failedUploads.equals(result.failedUploads);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTarget(), successfulUploads, failedUploads);
+    }
+
+    @Override
+    public String toString() {
+        return "AttachmentUploadResult{" +
+                "target=" + target +
+                ", successfulUploads=" + successfulUploads +
+                ", failedUploads=" + failedUploads +
+                '}';
+    }
+
+    /**
+     * Returns a copy of this object that contains the given attachment item as
+     * a successful upload.
+     *
+     * @param updatedRelease the updated release entity
+     * @param item           the item that was uploaded successfully
+     * @return the modified copy of this object
+     */
+    AttachmentUploadResult addSuccessfulUpload(SW360Release updatedRelease, AttachmentUploadRequest.Item item) {
+        Set<AttachmentUploadRequest.Item> updatedSet = new HashSet<>(successfulUploads);
+        updatedSet.add(item);
+        return new AttachmentUploadResult(updatedRelease, Collections.unmodifiableSet(updatedSet), failedUploads);
+    }
+
+    /**
+     * Returns a copy of this object that contains the given attachment item as
+     * a failed upload, together with the corresponding exception.
+     *
+     * @param item      the item that could not be uploaded
+     * @param exception the exception causing the upload to fail
+     * @return the modified copy of this object
+     */
+    AttachmentUploadResult addFailedUpload(AttachmentUploadRequest.Item item, Throwable exception) {
+        Map<AttachmentUploadRequest.Item, Throwable> updatedMap = new HashMap<>(failedUploads);
+        updatedMap.put(item, exception);
+        return new AttachmentUploadResult(getTarget(), successfulUploads, Collections.unmodifiableMap(updatedMap));
+    }
+}
---
+++ modules/sw360/sw360-client/src/test/java/org/eclipse/sw360/antenna/sw360/client/adapter/AttachmentUploadResultTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) Bosch.IO GmbH 2020.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.sw360.antenna.sw360.client.adapter;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.eclipse.sw360.antenna.sw360.client.rest.resource.attachments.SW360AttachmentType;
+import org.eclipse.sw360.antenna.sw360.client.rest.resource.releases.SW360Release;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class AttachmentUploadResultTest {
+    @Test
+    public void testEquals() {
+        EqualsVerifier.forClass(AttachmentUploadResult.class)
+                .withNonnullFields("successfulUploads", "failedUploads")
+                .verify();
+    }
+
+    @Test
+    public void testToString() {
+        Path successPath = Paths.get("success.txt");
+        Path failurePath = Paths.get("error.doc");
+        Throwable exception = new IOException("Failed upload");
+        SW360Release release = new SW360Release();
+        release.setName("uploadTargetRelease");
+        AttachmentUploadResult result = new AttachmentUploadResult(release)
+                .addSuccessfulUpload(release,
+                        new AttachmentUploadRequest.Item(successPath, SW360AttachmentType.SCREENSHOT))
+                .addFailedUpload(new AttachmentUploadRequest.Item(failurePath,
+                        SW360AttachmentType.SOURCE_SELF), exception);
+        String s = result.toString();
+
+        assertThat(s)
+                .contains(successPath.toString(), failurePath.toString(), exception.getMessage(), release.toString());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testSuccessUploadsNotModifiableInitial() {
+        AttachmentUploadResult result = new AttachmentUploadResult(new SW360Release());
+
+        result.successfulUploads()
+                .add(new AttachmentUploadRequest.Item(Paths.get("p"), SW360AttachmentType.SOURCE));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testSuccessUploadsNotModifiableWhenFilled() {
+        AttachmentUploadResult result = new AttachmentUploadResult(new SW360Release())
+                .addSuccessfulUpload(new SW360Release(),
+                        new AttachmentUploadRequest.Item(Paths.get("p1"), SW360AttachmentType.SCREENSHOT));
+
+        result.successfulUploads()
+                .add(new AttachmentUploadRequest.Item(Paths.get("p2"), SW360AttachmentType.SOURCE));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testFailedUploadsNotModifiableInitial() {
+        AttachmentUploadResult result = new AttachmentUploadResult(new SW360Release());
+
+        result.failedUploads()
+                .put(new AttachmentUploadRequest.Item(Paths.get("p"), SW360AttachmentType.SOURCE_SELF),
+                        new Exception());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testFailedUploadsNotModifiableWhenFilled() {
+        AttachmentUploadResult result = new AttachmentUploadResult(new SW360Release())
+                .addFailedUpload(new AttachmentUploadRequest.Item(Paths.get("p1"),
+                        SW360AttachmentType.SOURCE_SELF), new Exception("e1"));
+
+        result.failedUploads()
+                .put(new AttachmentUploadRequest.Item(Paths.get("p2"), SW360AttachmentType.SOURCE_SELF),
+                        new Exception("e2"));
+    }
+}
