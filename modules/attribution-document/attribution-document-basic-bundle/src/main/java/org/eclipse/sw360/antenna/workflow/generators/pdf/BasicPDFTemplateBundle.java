acbfb7762828458db58f727c7f82708d6ea1dfba
Onur Demirci <onur.demirci@bosch.io>
2020-06-30 08:51:46 +0200

    Add the open source font NotoSans in attribution-document-basic-bundle
    The AttributionDocumentGenerator uses the Times-Roman font if no font is
    provided in the template bundle. To avoid the use of a default font
    which doesn't support some glyphs, the open source font NotoSans is
    added to the basic template bundle. In addition, the template PDF files
    were edited with the new font and re-exported.
    
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

--- modules/attribution-document/attribution-document-basic-bundle/src/main/java/org/eclipse/sw360/antenna/workflow/generators/pdf/BasicPDFTemplateBundle.java
+++ modules/attribution-document/attribution-document-basic-bundle/src/main/java/org/eclipse/sw360/antenna/workflow/generators/pdf/BasicPDFTemplateBundle.java
@@ -10,16 +10,24 @@
  */
 package org.eclipse.sw360.antenna.workflow.generators.pdf;
 
+import org.apache.commons.io.IOUtils;
 import org.eclipse.sw360.antenna.attribution.document.core.TemplateBundle;
 
-import java.io.InputStream;
+import java.io.*;
 import java.util.Objects;
+import java.util.Optional;
 
 public class BasicPDFTemplateBundle implements TemplateBundle {
     private static final String TITLE_TEMPLATE = "/templates/basic_title.pdf";
     private static final String COPYRIGHT_TEMPLATE = "/templates/basic_copyright.pdf";
     private static final String CONTENT_TEMPLATE = "/templates/basic_content.pdf";
     private static final String BACKPAGE_TEMPLATE = "/templates/basic_back.pdf";
+    private static final String SANS_FONT = "/templates/NotoSans-Regular.ttf";
+    private static final String SANS_BOLD_FONT = "/templates/NotoSans-Bold.ttf";
+    private static final String SANS_BOLD_ITALIC_FONT = "/templates/NotoSans-BoldItalic.ttf";
+    private static final String SANS_ITALIC_FONT = "/templates/NotoSans-Italic.ttf";
+
+
 
     @Override
     public String key() {
@@ -44,6 +52,26 @@ public class BasicPDFTemplateBundle implements TemplateBundle {
     @Override
     public InputStream loadBackPageTemplate() {
         return loadFromClassPath(BACKPAGE_TEMPLATE);
+    }
+
+    @Override
+    public Optional<InputStream> loadSansFont() {
+        return Optional.of(loadFromClassPath(SANS_FONT));
+    }
+
+    @Override
+    public Optional<InputStream> loadSansBoldFont() {
+        return Optional.of(loadFromClassPath(SANS_BOLD_FONT));
+    }
+
+    @Override
+    public Optional<InputStream> loadSansBoldItalicFont() {
+        return Optional.of(loadFromClassPath(SANS_BOLD_ITALIC_FONT));
+    }
+
+    @Override
+    public Optional<InputStream> loadSansItalicFont() {
+        return Optional.of(loadFromClassPath(SANS_ITALIC_FONT));
     }
 
     private InputStream loadFromClassPath(String resource) {
